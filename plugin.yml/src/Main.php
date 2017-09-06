523ed3dc33b2be46d822f357e23b22b4b40a362e
Pluginer_Enc <z0502281868@gmail.com>
2017-09-06 10:10:40 -0800

    Update Main.php

--- plugin.yml/src/Main.php
+++ plugin.yml/src/Main.php
@@ -1,1 +1,20 @@
-
+name: KnockBack
+author: rezex._.
+version: 1.1.0
+api: [3.0.0]
+main: \GameManager\Main
+commands: 
+    games:
+      description: "By rezex._."
+      usage: "/kn [game]"
+      permission: gamemanager.true
+permissions:
+    gamemanager.true:
+      default: true
+    gamemanager.op:
+      default: op
+    gamemanager.false:
+      default: false
+    gamemanager.notop:
+      default: !op
+...
